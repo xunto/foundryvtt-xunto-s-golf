2fe81a75482707d2f764c1975c59f3e286e8519f
xunto <xunto.orlov@gmail.com>
2023-04-15 02:18:12 +0300

    Use power.

--- src/physics.ts
+++ src/physics.ts
@@ -1,10 +1,14 @@
 import { Vector } from "ts-matrix";
 
 export function pushTarget(actor: any, target: any, power: number) {
+    let gridSize = game.canvas.scene.grid.size;
+    let maxDistance = 10 * gridSize;
+    let actualDistance = maxDistance * (power/100);
+
     let actorPos = new Vector([actor.x, actor.y]);
     let targetPos = new Vector([target.x, target.y]);
 
-    let movement = targetPos.substract(actorPos).normalize().scale(500);
+    let movement = targetPos.substract(actorPos).normalize().scale(actualDistance);
 
     let newPos = targetPos.add(movement);
 
