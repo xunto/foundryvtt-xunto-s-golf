730aa30641f7c8594f09f80b3fc9d8cad282cf92
xunto <xunto.orlov@gmail.com>
2023-04-15 02:20:17 +0300

    Separate distance.

--- src/physics.ts
+++ src/physics.ts
@@ -1,16 +1,20 @@
 import { Vector } from "ts-matrix";
 
 export function pushTarget(actor: any, target: any, power: number) {
-    let gridSize = game.canvas.scene.grid.size;
-    let maxDistance = 10 * gridSize;
-    let actualDistance = maxDistance * (power/100);
+    let distance = calculatePushDistance(power);
 
     let actorPos = new Vector([actor.x, actor.y]);
     let targetPos = new Vector([target.x, target.y]);
 
-    let movement = targetPos.substract(actorPos).normalize().scale(actualDistance);
+    let movement = targetPos.substract(actorPos).normalize().scale(distance);
 
     let newPos = targetPos.add(movement);
 
     target.update({"x": newPos.values[0], "y": newPos.values[1]});
+}
+
+function calculatePushDistance(power: number): number {
+    let gridSize = game.canvas.scene.grid.size;
+    let maxDistance = 10 * gridSize;
+    return maxDistance * (power/100);
 }
